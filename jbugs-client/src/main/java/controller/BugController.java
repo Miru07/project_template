9140ee70a20833c1e256d411f14f93c32db63b38
Miruna Dinu <mirunaralucadinu@ymail.com>
2019-08-27 23:25:39 +0300

    Update response changed from BugDTO to OK

--- jbugs-client/src/main/java/controller/BugController.java
+++ jbugs-client/src/main/java/controller/BugController.java
@@ -68,11 +68,12 @@ public class BugController extends HttpServlet {
     @Consumes(MediaType.TEXT_PLAIN)
     public Response updateBugStatus(@PathParam("id") Integer bugID, String newStatus) {
 
+
         try {
             BugDTO updateResult = bugManagerRemote.updateBugStatus(newStatus, bugID);
-            return Response.status(Response.Status.OK).entity(updateResult).build();
+            return Response.status(Response.Status.OK).entity("OK").build();
         } catch (BusinessException e) {
-            return Response.status(500).entity(e.getErrorCode()).build();
+            return Response.status(500).entity(e).build();
         }
     }
 }
