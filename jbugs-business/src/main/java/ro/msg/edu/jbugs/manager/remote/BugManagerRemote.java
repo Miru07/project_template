4ba2f65cf11004a6526e11282001dada77559742
Klaus Siegel <klaussiegel2@gmail.com>
2019-08-30 10:48:37 +0300

    GetBugById + test fixed .

--- jbugs-business/src/main/java/ro/msg/edu/jbugs/manager/remote/BugManagerRemote.java
+++ jbugs-business/src/main/java/ro/msg/edu/jbugs/manager/remote/BugManagerRemote.java
@@ -3,7 +3,6 @@ package ro.msg.edu.jbugs.manager.remote;
 import ro.msg.edu.jbugs.dto.BugAttachmentWrapperDTO;
 import ro.msg.edu.jbugs.dto.BugDTO;
 import ro.msg.edu.jbugs.dto.BugViewDTO;
-import ro.msg.edu.jbugs.dto.UserDTO;
 import ro.msg.edu.jbugs.exceptions.BusinessException;
 
 import javax.ejb.Remote;
@@ -19,9 +18,10 @@ public interface BugManagerRemote {
 
     //List<BugDTO> findBugsCreatedBy(UserDTO userDTO);
 
-    BugDTO updateBugStatus(String newStatus, int bugId) throws BusinessException;
     List<BugDTO> getAllBugs();
+
     BugDTO closeBug(int bugID) throws BusinessException;
+
     BugViewDTO getBugViewDTO();
 
     BugDTO getBugById(Integer id);
